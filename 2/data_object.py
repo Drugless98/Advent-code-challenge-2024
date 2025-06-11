1ba399c128c3515452588bc75f0ab3ff39ac1da7
andreasMN <amn@sonderuprevisorer.dk>
2025-06-11 14:12:33 +0200

    cleanup

--- 2/data_object.py
+++ 2/data_object.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 @dataclass
 class Between():
@@ -8,16 +8,17 @@ class Between():
     def __post_init__(self): 
         self.Ascending = self.first_level < self.next_level if abs(self.first_level - self.next_level) < 4 else False
         self.Descending= self.first_level > self.next_level if abs(self.first_level - self.next_level) < 4 else False
-        self.Equal     = self.first_level== self.next_level
 
 @dataclass
 class Report:
     levels: list[str]
 
-    def __post_init__(self): #: make the "in between levels" object
+    def __post_init__(self):
+        #: make the "in between levels" object
         self.between_levels = [Between(int(l_from), int(l_too)) for l_from, l_too in zip(self.levels[:-1], self.levels[1:])]
         self.Ascending_count  = [i.Ascending  for i in self.between_levels].count(True)
         self.Descending_count = [i.Descending for i in self.between_levels].count(True)
 
+        #: Check if report is safe with and without damper
         self.Safe       = self.Ascending_count == len(self.levels) - 1 or self.Descending_count == len(self.levels) - 1
         self.SafeDamper = self.Ascending_count == len(self.levels) - 2 or self.Descending_count == len(self.levels) - 2
